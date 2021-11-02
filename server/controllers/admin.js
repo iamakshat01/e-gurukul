49d9fb1edb62868f8572c8a1c65666356f28b059
Akshat mangal <mangalakshat01@gmail.com>
2021-11-02 15:27:10 +0530

    Added batch creation and fixed student batch issue

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -2,6 +2,7 @@ const parseCSVBuffer = require('../utility/parseCSVData');
 const User = require('../models/user');
 const Faculty = require('../models/faculty');
 const Student = require('../models/student');
+const Batch = require('../models/batch');
 
 // registering faculty
 exports.registerFaculty = async (req, res, next) => {
@@ -41,6 +42,10 @@ exports.registerStudent = async (req, res, next) => {
     try {
         const studentData = await parseCSVBuffer(req.file.buffer);
         
+        const batch = await Batch.findOne({
+            batch_code: req.body.batch
+        })
+
         studentData.forEach(async function (student) {
             
             const user = await User.create({
@@ -52,12 +57,28 @@ exports.registerStudent = async (req, res, next) => {
             const newStudent = await Student.create({
                 user_id: user._id,
                 roll_no: student.roll_no,
+                batch: batch._id,
                 personal_info: student,
             })
+
+            // console.log(batch, newStudent);
         });
 
         return res.status(201).json('Student list succesfully added');
 
+    } catch(err) {
+        return next({
+            status: 400,
+            message: err.message
+        })
+    }
+}
+
+
+exports.createBatch = async (req,res,next) => {
+    try {
+        const batch = await Batch.create(req.body);
+        return res.status(201).json('Batch Created Successfully');
     } catch(err) {
         return next({
             status: 400,
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const parseFile = require('../utility/parseFile')
-const {registerFaculty, registerStudent} = require('../controllers/admin')
+const {registerFaculty, registerStudent, createBatch} = require('../controllers/admin')
 
 
 // for registering faculty
@@ -9,5 +9,8 @@ router.post('/register/faculty',parseFile('faculty'),registerFaculty);
 
 // for registering student
 router.post('/register/student',parseFile('student'),registerStudent);
+
+// for creating a batch
+router.post('/batch/register',createBatch);
 
 module.exports = router;
