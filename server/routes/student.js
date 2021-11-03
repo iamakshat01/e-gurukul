c58695280f5b0bf0776164c4de444e5adaead81d
ImAkhil24 <akhilbadoni24@gmail.com>
2021-11-03 11:26:54 +0530

    added student/classroom/:classroom_id route

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const studentActiveClassrooms = require("../controllers/studentActiveClassrooms");
+const student = require("../controllers/student");
 
 const dummyAuth = (req, res, next) => {
   const user = {
-    _id: "61810842dfaf5357a7ebc8c4",
+    _id: "6182240fc391cdaa470c0082",
     role: "student",
   };
   req.user = user;
   next();
 };
 // add auth middleware
-router.get("/activeClassrooms", dummyAuth, studentActiveClassrooms);
+router.get("/activeClassrooms", dummyAuth, student.activeClassrooms);
+router.get("/classrooms/:classroom_id", dummyAuth, student.classroom);
 
 module.exports = router;
