1430f6cd5b95851ddb00220885a0904c24ec9b36
Amit Sahu <amitsahu.ltp@gmail.com>
2021-11-07 17:56:37 +0530

    Setup Basic Routes Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+  BrowserRouter as Router
 } from "react-router-dom";
-import Login from "./components/Login"
 import { ThemeProvider } from '@mui/material/styles';
+
 import theme from './theme';
+
+import Main from "./components/Main";
 
 
 
@@ -14,14 +14,9 @@ function App() {
 
   return (
     <Router>
-       <ThemeProvider theme={theme}>
-
-        <Routes>
-          <Route path='/login' element={<Login/>} />
-        </Routes>
-
+      <ThemeProvider theme={theme}>
+        <Main />
       </ThemeProvider>
-
     </Router>
   );
 }
