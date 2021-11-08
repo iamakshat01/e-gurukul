3ed6acab553e3918115614897236a364acb94b6c
Akshat mangal <mangalakshat01@gmail.com>
2021-11-08 18:20:42 +0530

    Improved ui of admin dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import {call,setToken} from '../services/api'
+import {call,setToken} from '../services/api';
+import { makeStyles } from '@mui/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,20 +9,28 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Circle from './Loading'
 import Grid from '@mui/material/Grid';
+import ClassIcon from '@mui/icons-material/Class';
+import TodayIcon from '@mui/icons-material/Today';
 
 
 function SingleCard(props) {
     const {info}=props;
     return (
-        <Grid item xs={6} xl={2}>
+        <Grid item sm={6} xl={2}>
             <Card variant="outlined">
                 <CardContent>
-                    <Typography variant="h5" component="div">
-                    {info.batch_code}
-                    </Typography>
-                    <Typography sx={{ mb: 0.5 }} color="text.secondary">
-                    {info.start_year} - {info.end_year}
-                    </Typography>
+                    <Grid container direction="row" alignItems="center">
+                        <ClassIcon /> 
+                        <Typography variant="h5">
+                        {" "} {info.batch_code}
+                        </Typography>
+                    </Grid>
+                    <Grid container direction="row" alignItems="center" mt={2}>
+                        <TodayIcon /> 
+                        <Typography>
+                        {" "} {info.start_year} - {info.end_year}
+                        </Typography>
+                    </Grid>
                 </CardContent>
                 <CardActions>
                     <Button size="small">Manage Batch</Button>
@@ -35,6 +44,7 @@ export default function Dashboard(props) {
 
     const [batches, setBatches] = useState([]);
     const [isLoading, setLoading] = useState(true);
+
     useEffect(() => {
         setToken(localStorage.getItem('jwtToken'));
         call('get','admin/batch').then((data) => {
@@ -56,7 +66,7 @@ export default function Dashboard(props) {
         const cardList = batches.map( (batch) => <SingleCard info={batch} /> )
         return (
             <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
+                <Grid container spacing={2} m={2}>
                     {cardList}
                 </Grid>
             </Box>
