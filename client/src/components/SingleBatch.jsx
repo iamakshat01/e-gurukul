5386a728b5eff86a14534ec7009b3de766951fae
Akshat mangal <mangalakshat01@gmail.com>
2021-11-12 13:25:23 +0530

    Student deletion completed

--- client/src/components/SingleBatch.jsx
+++ client/src/components/SingleBatch.jsx
@@ -5,26 +5,46 @@ import Circle from './Loading';
 import { DataGrid } from '@mui/x-data-grid';
 import {Grid,Container,Typography,Divider,IconButton} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-
-const MatDelete = ({ index }) => {
-
-    const handleDeleteClick = () => {
-        console.log(index);
-    }
-
-    return (
-
-        <IconButton onClick={handleDeleteClick} >
-            <DeleteIcon fontSize="small" />
-        </IconButton>
-    )
-           
-};
+import Notification from './Utility/Notifications';
+import ConfirmDialog from './Utility/ConfirmDialog';
 
 export default function SingleBatch(props) {
+    
     let { batch_id } = useParams();
     const [batchdetails,setDetails] = useState();
     const [isLoading, setLoading] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [userId, setuserId] = useState('');
+    const [notify, setNotify] = useState({ isOpen:false, message:'', type:''});
+
+    const handleDeleteClick = (Id) => {
+        setOpen(true);
+        setuserId(Id)
+    }
+    
+    const onConfirm = () => {
+        
+        setOpen(false);
+
+        // server call
+
+        call('delete','admin/'+userId).then((data)=>{
+            setNotify({
+                isOpen: true,
+                message: 'User Deleted Successfully',
+                type: 'success'
+            })
+        }).catch((err)=>{
+            console.log(err);
+        })
+
+    }
+
+    const onCancel = () => {
+        setOpen(false);
+    }
+
+
     useEffect(() => {
         setToken(localStorage.getItem('jwtToken'));
         call('get','admin/batch/'+batch_id).then((data) => {
@@ -34,7 +54,8 @@ export default function SingleBatch(props) {
         }).catch((err) => {
             console.log(err);
         })
-    }, [batch_id])
+    }, [batch_id,open])
+
     const columns = [
         { field: 'roll_no', headerName: 'Roll No', width: 160 },
         {
@@ -64,7 +85,9 @@ export default function SingleBatch(props) {
             renderCell: (params) => {
                 return (
                     <div>
-                        <MatDelete index={params.row._id} />
+                        <IconButton onClick={() => handleDeleteClick(params.row.user_id)} >
+                            <DeleteIcon fontSize="small" />
+                        </IconButton>
                      </div>
                 );
              }
@@ -85,7 +108,12 @@ export default function SingleBatch(props) {
                     <Divider />
                 </Grid>
             </Grid>
+            <Notification
+                notify={notify}
+                setNotify={setNotify}
+            />
             <Grid container justifyContent="center" sx={{height: "65vh"}} mt={4}>
+                <ConfirmDialog open={open} onConfirm={onConfirm} onCancel={onCancel} message={"Delete User"} type={"warning"}/>
                 <Grid item xs={12} sm={12} lg={6} xl={6} >
                     <DataGrid
                         rows={batchdetails.students}
